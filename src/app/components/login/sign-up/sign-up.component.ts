dc978843f32a6ee84d27ad3bdb44ce49b63c2da8
Stepan_Prokopiak <Stepan_Prokopiak@epam.com>
2018-04-26 17:09:51 +0300

    Added routes

---
+++ src/app/components/login/sign-up/sign-up.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss']
+})
+export class SignUp implements OnInit {
+
+    ngOnInit(){
+
+    }
+}
