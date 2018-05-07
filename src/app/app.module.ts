f469bb0cc9343fff5c6ef87a17e63e83bd53cfe3
Stepan_Prokopiak <Stepan_Prokopiak@epam.com>
2018-05-07 17:02:02 +0300

    Added authorization using gmail account

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AppRoutes } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseConfig } from '../environments/firebase.config';
+import { CoreModule } from './core/core.module';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
 import { Home } from './components/home/home.component';
@@ -9,18 +13,26 @@ import { SignUp } from './components/login/sign-up/sign-up.component';
 import { SignIn } from './components/login/sign-in/sign-in.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Home,
-    SignUp,
-    SignIn
-  ],
-  imports: [
-    CommonModule, 
-    BrowserModule,
-    AppRoutes
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        Home,
+        SignUp,
+        SignIn
+    ],
+    imports: [
+        CommonModule, 
+        BrowserModule,
+        AppRoutes,
+        AngularFireModule.initializeApp(FirebaseConfig.firebase),
+        CoreModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+
+export class AppModule {
+    constructor( private afs: AngularFirestore) {
+        const settings = {timestampsInSnapshots: true};
+        afs.firestore.settings(settings);
+    }
+}
